e649e69ecd1db811631a7f0838dcd191f8f52aa3
bergs533 <54900195+bergs533@users.noreply.github.com>
2020-04-17 17:33:39 -0500

    Add files via upload

---
+++ mini2/mini2.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.button').click(function () {
+        $(".button").addClass("button-move");
+        $(".broccoli").addClass("broccoli-move");
+        $(".banana").addClass("banana-move");
+        $(".carrot").addClass("carrot-move");
+        $(".apple").addClass("apple-move");
+        $(".stay").addClass("stay-move");
+        $(".healthy").addClass("healthy-move");
+    });
+});
